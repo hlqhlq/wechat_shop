08ceccb103a373b3dcbbe1f0d3fc55edac61d7b7
HLQ <heliqingwork@hotmail.com>
2019-04-25 21:55:34 +0800

    1.解决登录失败，账号信息回显问题 2.把各列表布局修改，让屏幕缩小能显示数据

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,32 +3,29 @@ layui.use(['form','layer','jquery'],function(){
         layer = parent.layer === undefined ? layui.layer : top.layer
         $ = layui.jquery;
 
-    $(".loginBody .seraph").click(function(){
-        layer.msg("这只是做个样式，至于功能，你见过哪个后台能这样登录的？还是老老实实的找管理员去注册吧",{
-            time:5000
-        });
-    })
-
     //登录按钮
     form.on("submit(login)",function(data){
         var that=$(this)
         that.text("登录中...").attr("disabled","disabled").addClass("layui-disabled");
         $.ajax({
             url:context+'/pc/login',
-            type:'post',
-            data:$("#app-form").serialize(),
+            method:'post',
+            dataType:'json',
+            data:{'username':data.field.username,'password':data.field.password},
             success:function (result) {
                 if(result.code==0){
                     layer.msg("登陆成功",{icon:1,shade:0.4,time:3000})
                     location.href = context + 'pc/main'
                 }else{
                     that.text("登录").removeAttr("disabled").removeClass("layui-disabled");
-                    layer.msg("登陆失败",{icon:5,shade:0.4,time:2000})
+                    layer.msg("登陆失败",{icon:5,shade:0.4,time:3000})
+                    // location.href = context + 'pc/index'
 
                 }
             }
 
         })
+        return false;
 
     })
 
